77183e5fd8af4c645072fc2ec6d23665ac855bf7
manfouothierno <121230773+manfouothierno@users.noreply.github.com>
2024-06-29 18:30:23 +0000

    updates

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -102,12 +102,12 @@ export default function Login() {
               <div className="grid gap-2">
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
-                  <Link
+                  {/* <Link
                     href="auth/forgot_password"
                     className="ml-auto inline-block text-sm underline"
                   >
                     Forgot your password?
-                  </Link>
+                  </Link> */}
                 </div>
                 <Input
                   id="password"
